023c9251473177d763064454f0b0051b0bbd2410
Hanul Kim <s2122@e-mirim.hs.kr>
2023-03-21 15:38:40 +0900

    처음 설정

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -1,7 +1,10 @@
 package kr.hs.study;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
         System.out.println("Hello world!");
     }
 }
