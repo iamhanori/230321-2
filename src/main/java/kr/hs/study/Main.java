2cf871add0f726199aa903a9a092879d34c1125d
Hanul Kim <s2122@e-mirim.hs.kr>
2023-03-21 15:50:49 +0900

    Lombok의 데이터 어노테이션

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -1,10 +1,15 @@
 package kr.hs.study;
 
+import kr.hs.study.beans.TestBean1;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("config.xml");
         System.out.println("Hello world!");
+
+        TestBean1 obj1 = ctx.getBean("t1", TestBean1.class);
+        System.out.println("obj1 : " + obj1);
+        ctx.close();
     }
 }
---
+++ src/main/java/kr/hs/study/beans/TestBean1.java
@@ -0,0 +1,12 @@
+package kr.hs.study.beans;
+
+import lombok.Data;
+@Data // 어노테이션 : setter, getter, toString(), constructor
+public class TestBean1 {
+    private int Data1;
+    private double Data2;
+    private boolean Data3;
+    private String Data4;
+
+
+}
