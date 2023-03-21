4920c9a1720571b428bb895ee23906c8f998a7c0
Hanul Kim <s2122@e-mirim.hs.kr>
2023-03-21 16:20:04 +0900

    property!

--- src/main/java/kr/hs/study/Main.java
+++ src/main/java/kr/hs/study/Main.java
@@ -1,6 +1,7 @@
 package kr.hs.study;
 
 import kr.hs.study.beans.TestBean1;
+import kr.hs.study.beans.TestBean2;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
@@ -10,6 +11,13 @@ public class Main {
 
         TestBean1 obj1 = ctx.getBean("t1", TestBean1.class);
         System.out.println("obj1 : " + obj1);
+
+        TestBean2 obj2 = ctx.getBean("t22", TestBean2.class);
+        System.out.println("obj2 : " + obj2);
+
+        TestBean2 obj3 = ctx.getBean("t23", TestBean2.class);
+        System.out.println("obj3 : " + obj3);
+
         ctx.close();
     }
 }
